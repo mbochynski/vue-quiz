252264c6ffcea6c7dea73b9d65c5eddfdbdb8c87
Mateusz Bochynski <matbochynski@gmail.com>
2020-05-10 21:58:41 +0200

    Created store draft

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,52 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    currentQuestionNumber: 0,
+    questions: [],
+    answers: [],
+    loading: false,
+  },
+  mutations: {
+    addQuestions(state, questions) {
+      state.questions.push(questions);
+    },
+    submitAnswer(state, currentQuestionNumber, currentAnswer) {
+      state.currentQuestionNumber = currentQuestionNumber + 1;
+      state.answers.push(currentAnswer);
+    },
+    startQuizLoader(state) {
+      state.loading = true;
+    },
+    stopQuizLoader(state) {
+      state.loading = false;
+    },
+  },
+  actions: {
+    submitAnswer({ commit, getters }) {
+      commit(
+        "submitAnswer",
+        getters.getCurrentQuestionNumber(),
+        getters.getCurrentAnswer()
+      );
+    },
+    fetchNewQuiz({ commit }) {
+      commit("startQuizLoader");
+      return fetch(
+        "https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          commit("addQuestions", data.results);
+          commit("stopQuizLoader");
+        });
+    },
+  },
+  getters: {
+    getCurrentQuestionNumber: (state) => state.currentQuestionNumber,
+    getCurrentQuestion: (state) => {
+      /* TODO */
+    },
+  },
+  modules: {},
 });
---
+++ src/views/Quiz.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="quiz">
+    <h1>This is an quiz page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz",
+  async created() {
+    await this.$store.dispatch("fetchNewQuiz");
+  }
+};
+</script>

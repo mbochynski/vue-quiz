f3894ba3d3c614f5009dcff0dfda9ae5e374ad32
MBochynski <matbochynski@gmail.com>
2020-05-24 22:17:34 +0200

    Add custom component with vmodel

---
+++ src/components/Options.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label v-for="option in options" v-bind:key="option">
+      <input
+        type="radio"
+        name="answers"
+        v-bind:value="option"
+        v-model="selectedAnswer"
+      />
+      {{ option }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Options",
+  props: ["options", "value"],
+  data: function() {
+    return {
+      selectedAnswer: this.value
+    };
+  },
+  watch: {
+    selectedAnswer() {
+      this.$emit("input", this.selectedAnswer);
+    }
+  }
+};
+</script>
--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -2,8 +2,12 @@
   <div class="questions">
     <h1>This is a question page</h1>
     <h2>
-      Question {{ currentQuestionNumber }} out of {{ totalQuestionNumber }}
+      Questions {{ currentQuestionNumber }} out of {{ totalQuestionNumber }}
     </h2>
+    <Options
+      :options="currentQuestionAnswers"
+      v-model="selectedAnswer"
+    ></Options>
     <pre>{{ currentQuestion }}</pre>
     <pre>{{ currentQuestionAnswers }}</pre>
   </div>
@@ -11,8 +15,18 @@
 
 <script>
 import { shuffleArray } from "../utils";
+import Options from "../components/Options.vue";
+
 export default {
   name: "Questions",
+  components: {
+    Options
+  },
+  data: function() {
+    return {
+      selectedAnswer: null
+    };
+  },
   computed: {
     currentQuestion() {
       return this.$store.getters.getCurrentQuestion;
@@ -24,6 +38,8 @@ export default {
       const { correct_answer, incorrect_answers } = this.currentQuestion;
 
       const answers = [correct_answer, ...incorrect_answers];
+      // TODO answers generation should be moved to store when quiz is initialized
+      // TODO this component should not be aware of correct/incorrect answers - it should be handled internally by store
       shuffleArray(answers);
       return answers;
     },
